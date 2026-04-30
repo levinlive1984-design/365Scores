28945296df52234094510cfb5e74c1d71f691dd3
levinlive1984-design <levinlive1984@gmail.com>
2026-04-30 10:08:47 +0800

    Update espn_utils.py

--- espn_utils.py
+++ espn_utils.py
@@ -2,15 +2,24 @@ import requests
 from datetime import datetime
 import pytz
 
-def get_espn_scoreboard(sport, league):
+def get_espn_scoreboard(sport, league, target_date=None):
     """
-    從 ESPN 抓取指定的賽事數據
+    從 ESPN 抓取指定賽事與日期的數據
+    target_date: datetime.date 物件
     """
     tw_tz = pytz.timezone('Asia/Taipei')
+    
+    # 基本 URL
     url = f"https://site.api.espn.com/apis/site/v2/sports/{sport}/{league}/scoreboard"
     
+    # 如果有傳入日期，將日期格式化為 YYYYMMDD 並加入參數
+    params = {}
+    if target_date:
+        params['dates'] = target_date.strftime('%Y%m%d')
+    
     try:
-        res = requests.get(url, timeout=10).json()
+        # 加入 params 進行請求
+        res = requests.get(url, params=params, timeout=10).json()
         events = res.get('events', [])
         parsed_data = []
         
@@ -19,8 +28,8 @@ def get_espn_scoreboard(sport, league):
             utc_time = datetime.strptime(ev['date'], "%Y-%m-%dT%H:%MZ").replace(tzinfo=pytz.utc)
             local_time = utc_time.astimezone(tw_tz).strftime('%H:%M')
             
-            # 2. 狀態判定 (改用 state 狀態機，更精確)
-            state = ev['status']['type']['state'] # pre, in, post
+            # 2. 狀態判定
+            state = ev['status']['type']['state'] 
             if state == 'post':
                 status_text = "已結束"
             elif state == 'in':
@@ -35,7 +44,6 @@ def get_espn_scoreboard(sport, league):
             away = comp['competitors'][0]
             home = comp['competitors'][1]
             
-            # 取得比分，若尚未開始則顯示 "-"
             away_score = away.get('score', '0')
             home_score = home.get('score', '0')
 
