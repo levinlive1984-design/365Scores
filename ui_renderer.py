01273e9f4bdaaecf200bee2d267a255316f6bfb2
levinlive1984-design <levinlive1984@gmail.com>
2026-05-02 21:16:08 +0800

    Update ui_renderer.py

--- ui_renderer.py
+++ ui_renderer.py
@@ -26,7 +26,7 @@ def setup_cyber_css():
             }
 
             @keyframes fadeIn {
-                from { opacity: 0.1; }
+                from { opacity: 1; }
                 to   { opacity: 1; }
             }            
             
